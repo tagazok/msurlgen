a52e35d33b0655cc3dad0026b50a9e1437deb252
Olivier Mertens <olmertens@microsoft.com>
2022-06-16 17:50:31 +0200

    Add information to tracked devRel extension

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let url = "&nbsp;";
 let petId = "";
 let generatedUrl = "";
+let devRel = "";
 
 // Get DOM elements
 const generaterUrlDOM = document.querySelector("#generatedUrl");
 const petIdInputDOM = document.querySelector("#petId");
+const devRelInputDOM = document.querySelector("#devRel");
 const urlInputDOM = document.querySelector("#url");
 const copyToClipBoardDOM = document.querySelector("#copyToClipBoard")
 
@@ -18,26 +20,38 @@ copyToClipBoardDOM.addEventListener('click', async e => {
 });
 petIdInputDOM.addEventListener('keyup', getPetId);
 urlInputDOM.addEventListener('keyup', getUrl);
+devRelInputDOM.addEventListener('keyup', getDevRel);
 
 // Pre-fill PET ID if present in URL
 const params = new URLSearchParams(window.location.search)
 if (params.has('pet')) {
     petId = params.get('pet');
     petIdInputDOM.value = petId;
-
     if (params.has('lock') && params.get('lock') === 'true') {
         document.querySelector('#section-petId').style.display = "none";
+
+    }
+}
+if (params.has('devrel')) {
+    devRel = params.get('devrel');
+    devRelInputDOM.value = devRel;
+    if (params.has('lock') && params.get('lock') === 'true') {
+        document.querySelector('#section-devRel').style.display = "none";
     }
 }
 
 
-console.log(`PetId: ${petId}`);
+
 
 function getPetId() {
     petId = petIdInputDOM.value;
     generateURL();
 }
 
+function getDevRel() {
+    devRel = devRelInputDOM.value;
+    generateURL();
+}
 function getUrl() {
     url = urlInputDOM.value;
     generateURL();
@@ -45,12 +59,22 @@ function getUrl() {
 
 function generateURL() {
     generatedUrl = "&nbsp;";
+    devRel = `${devRel}`;
+    petId = `${petId}`;
     try {
         generatedUrl = new URL(url);
-        generatedUrl.searchParams.set('ocid', `AID${petId}`);
+        if( petId !=="")
+        {
+            generatedUrl.searchParams.set('ocid', `AID${petId}`);
+        }
+        if(devRel !== "") {
+            generatedUrl.searchParams.set('wt.mc_id', devRel);
+        }
 
     } catch (error) {
         generatedUrl = "&nbsp;";
+        console.log(`PetId: ${petId}`);
+        console.log(`DevRel: ${devRel}`);
     }
     generaterUrlDOM.innerHTML = generatedUrl;
 }
